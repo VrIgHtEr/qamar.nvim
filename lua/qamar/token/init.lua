898f7b6ed0aa55d26b797f9250a253adb6cd88c0
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-03-09 17:34:57 +0100

    refactor

--- lua/qamar/token/init.lua
+++ lua/qamar/token/init.lua
@@ -121,8 +121,7 @@ return function(buffer)
             for i = 1, la.size() do
                 local line = { (i - 1 == t.index) and '==> ' or '    ' }
                 local c = la[i]
-                local str = vim.inspect(c):gsub('\r\n', '\n'):gsub('\r', '\n'):gsub('\n%s*', ' ')
-                table.insert(line, str)
+                table.insert(line, (vim.inspect(c):gsub('\r\n', '\n'):gsub('\r', '\n'):gsub('\n%s*', ' ')))
                 table.insert(ret, table.concat(line))
             end
             if t.index == la.size() then
