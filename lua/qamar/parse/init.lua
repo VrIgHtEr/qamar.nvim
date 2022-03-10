7995d47843a68f9e36c30f9f1ae6d1695bd481bb
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-03-10 13:37:30 +0100

    finished expression parser

--- lua/qamar/parse/init.lua
+++ lua/qamar/parse/init.lua
@@ -1,9 +1,10 @@
+local node_types = require 'qamar.parse.types'
 local token_types = require 'qamar.token.types'
 local infix_token_type_mappings = require 'qamar.parse.infix_token_type_mappings'
 local prefix_token_type_mappings = require 'qamar.parse.prefix_token_type_mappings'
 local precedences = require 'qamar.parse.precedence'
 
-return function(tokenizer)
+local new_parser = function(tokenizer)
     local parser = {}
 
     local infix_parselet = function(self, left, token)
@@ -11,7 +12,36 @@ return function(tokenizer)
         if not right then
             return nil
         end
-        return { type = infix_token_type_mappings[token.type], left = left, right = right, pos = { left = left.pos.left, right = right.pos.right } }
+        return setmetatable({
+            type = infix_token_type_mappings[token.type],
+            left = left,
+            right = right,
+            pos = { left = left.pos.left, right = right.pos.right },
+        }, {
+            __tostring = function(node)
+                local ret = {}
+                local paren = node.left.type ~= node_types.name and node.left.type ~= node_types.number
+                if paren then
+                    table.insert(ret, '(')
+                end
+                table.insert(ret, tostring(node.left))
+                if paren then
+                    table.insert(ret, ')')
+                end
+                table.insert(ret, ' ')
+                table.insert(ret, node_types[node.type])
+                table.insert(ret, ' ')
+                paren = node.right.type ~= node_types.name and node.right.type ~= node_types.number
+                if paren then
+                    table.insert(ret, '(')
+                end
+                table.insert(ret, tostring(node.right))
+                if paren then
+                    table.insert(ret, ')')
+                end
+                return table.concat(ret)
+            end,
+        })
     end
 
     local prefix_parselet = function(self, token)
@@ -19,7 +49,24 @@ return function(tokenizer)
         if not operand then
             return nil
         end
-        return { type = prefix_token_type_mappings[token.type], operand = operand, pos = { left = token.pos.left, right = operand.pos.right } }
+        return setmetatable({
+            type = prefix_token_type_mappings[token.type],
+            operand = operand,
+            pos = { left = token.pos.left, right = operand.pos.right },
+        }, {
+            __tostring = function(node)
+                local ret = { node_types[node.type] }
+                local paren = node.operand.type ~= node_types.name and node.operand.type ~= node_types.number
+                if paren then
+                    table.insert(ret, '(')
+                end
+                table.insert(ret, tostring(node.operand))
+                if paren then
+                    table.insert(ret, ')')
+                end
+                return table.concat(ret)
+            end,
+        })
     end
 
     local infix_parselets = {
@@ -47,28 +94,55 @@ return function(tokenizer)
     }
 
     local prefix_parselets = {
+        [token_types.kw_not] = { precedence = precedences.unary, right_associative = false, parse = prefix_parselet },
+        [token_types.len] = { precedence = precedences.unary, right_associative = false, parse = prefix_parselet },
+        [token_types.sub] = { precedence = precedences.unary, right_associative = false, parse = prefix_parselet },
+        [token_types.bitnot] = { precedence = precedences.unary, right_associative = false, parse = prefix_parselet },
         [token_types.name] = {
             precedence = precedences.atom,
             right_associative = false,
             parse = function(_, token)
-                return { value = token.value, type = prefix_token_type_mappings[token.type], pos = token.pos }
+                return setmetatable({
+                    value = token.value,
+                    type = prefix_token_type_mappings[token.type],
+                    pos = token.pos,
+                }, {
+                    __tostring = function(node)
+                        return node.value
+                    end,
+                })
             end,
         },
         [token_types.number] = {
             precedence = precedences.atom,
             right_associative = false,
             parse = function(_, token)
-                return { value = token.value, type = prefix_token_type_mappings[token.type], pos = token.pos }
+                return setmetatable({
+                    value = token.value,
+                    type = prefix_token_type_mappings[token.type],
+                    pos = token.pos,
+                }, {
+                    __tostring = function(node)
+                        return node.value
+                    end,
+                })
             end,
         },
-        [token_types.kw_not] = { precedence = precedences.unary, right_associative = false, parse = prefix_parselet },
-        [token_types.len] = { precedence = precedences.unary, right_associative = false, parse = prefix_parselet },
-        [token_types.sub] = { precedence = precedences.unary, right_associative = false, parse = prefix_parselet },
-        [token_types.bitnot] = { precedence = precedences.unary, right_associative = false, parse = prefix_parselet },
     }
 
     local function fail()
         tokenizer.undo()
+    end
+
+    local function get_precedence()
+        local next = tokenizer.peek()
+        if next then
+            local infix = infix_parselets[next.type]
+            if infix then
+                return infix.precedence
+            end
+        end
+        return 0
     end
 
     function parser.parse_exp(precedence)
@@ -89,30 +163,40 @@ return function(tokenizer)
             return fail()
         end
 
-        token = tokenizer.peek()
-        if not token then
-            tokenizer.commit()
-            return left
-        end
-        local infix = infix_parselets[token.type]
-        if not infix then
-            tokenizer.commit()
-            return left
-        end
-        tokenizer.take()
+        while precedence < get_precedence() do
+            token = tokenizer.peek()
+            if not token then
+                tokenizer.commit()
+                return left
+            end
 
-        tokenizer.begin()
-        local ret = infix:parse(left, token)
-        if not ret then
-            tokenizer.undo()
-            tokenizer.undo()
-            return left
-        else
-            tokenizer.commit()
+            local infix = infix_parselets[token.type]
+            if not infix then
+                tokenizer.commit()
+                return left
+            end
+            tokenizer.begin()
+            tokenizer.take()
+            local right = infix:parse(left, token)
+            if not right then
+                tokenizer.undo()
+                tokenizer.undo()
+                return left
+            else
+                tokenizer.commit()
+                left = right
+            end
         end
+
         tokenizer.commit()
-        return ret
+        return left
     end
 
-    return parser
+    return parser.parse_exp
 end
+
+local ppp = new_parser(require 'qamar.token'(require 'qamar.token.buffer'(require('toolshed.util.string').codepoints 'a+b*3+4')))
+local parsed = ppp()
+print(parsed)
+
+return new_parser
