81a39d7cfffbd4850f3d2a993bfaeffd45fbf163
Cedric Mamo <vrighter@gmail.com>
2022-03-13 12:00:18 +0100

    refactor

--- lua/qamar/config.lua
+++ lua/qamar/config.lua
@@ -1,4 +1,4 @@
 local M = {}
 M.expression_display_modes = { prefix = 'prefix', infix = 'infix', postfix = 'postfix' }
-M.expression_display_mode = M.expression_display_modes.infix
+M.expression_display_mode = M.expression_display_modes.postfix
 return M
--- lua/qamar/init.lua
+++ lua/qamar/init.lua
@@ -1,15 +1,19 @@
 local qamar = {}
+local codepoints = require('toolshed.util.string').codepoints
+local parser = require 'qamar.parser'
+local tokenizer = require 'qamar.tokenizer'
+local char_stream = require 'qamar.tokenizer.char_stream'
+
+local function create_parser(str)
+    return parser(tokenizer(char_stream(codepoints(str))))
+end
 
 function qamar.run()
-    local ppp = require 'qamar.parser'(
-        require 'qamar.tokenizer'(require 'qamar.tokenizer.char_stream'(require('toolshed.util.string').codepoints 'a+-b*-3^((4 or 7)+6)^7+4+(7+5)'))
-    )
+    local ppp = create_parser 'a+-b*-3^((4 or 7)+6)^7+4+(7+5)'
     local parsed = ppp.expression()
     print(parsed)
     print '---------------------------------------------------------------------'
-    ppp = require 'qamar.parser'(
-        require 'qamar.tokenizer'(require 'qamar.tokenizer.char_stream'(require('toolshed.util.string').codepoints 'cedric.dingli.mamo:cedric'))
-    )
+    ppp = create_parser 'cedric.dingli.mamo:cedric'
     repeat
         parsed = ppp.funcname()
         print(vim.inspect(parsed))
