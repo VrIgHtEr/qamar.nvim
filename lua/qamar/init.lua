237a17cf7e1bac58bf9361bff0bd2102347c00d7
Cedric Mamo <vrighter@gmail.com>
2022-03-15 17:50:14 +0100

    refactor

--- lua/qamar/init.lua
+++ lua/qamar/init.lua
@@ -9,22 +9,10 @@ local function create_parser(str)
 end
 
 function qamar.run()
-    --[[
-    local ppp = create_parser 'a+-b*-3^((4 or 7)+6)^7+4+(7+5)'
-    local parsed = ppp.expression()
-    print(parsed)
-    print '---------------------------------------------------------------------'
-    ppp = create_parser 'local x1<close> = 7 + 2 if x1 == 3 then return x1 + 7, 5 end return x1 / 2'
-    repeat
-        parsed = ppp.chunk()
-        print(vim.inspect(parsed))
-        print '---------------------------------------------------------------------'
-    until parsed == nil
-    ]]
-    local txt = require('toolshed.util').read_file(vim.fn.stdpath 'data' .. '/site/pack/vrighter/opt/qamar.nvim/test.lua')
-    local ppp = create_parser(txt)
-    local parsed = ppp.chunk()
-    local roundtripstr = tostring(parsed)
+    local txt = require('toolshed.util').read_file(vim.fn.stdpath 'data' .. '/site/pack/vrighter/opt/qamar.nvim/lua/qamar/parser/init.lua')
+    local p = create_parser(txt)
+    local tree = p.chunk()
+    local roundtripstr = tostring(tree)
     print(roundtripstr)
     require('toolshed.util').write_file(vim.fn.stdpath 'data' .. '/site/pack/vrighter/opt/qamar.nvim/test.lua', roundtripstr)
 end
