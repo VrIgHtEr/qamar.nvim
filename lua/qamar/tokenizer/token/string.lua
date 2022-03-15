7872ba4c3c7b25cdc10b57fb1f2a5cc22dc4e415
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-03-15 13:07:15 +0100

    bugfix

--- lua/qamar/tokenizer/token/string.lua
+++ lua/qamar/tokenizer/token/string.lua
@@ -1,4 +1,5 @@
 local token = require 'qamar.tokenizer.types'
+local string = require'toolshed.util.string'
 
 local function tohexdigit(c)
     if c == '0' or c == '1' or c == '2' or c == '3' or c == '4' or c == '5' or c == '6' or c == '7' or c == '8' or c == '9' then
