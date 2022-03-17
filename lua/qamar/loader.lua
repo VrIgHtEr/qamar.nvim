04ceb2cbccce7b0b6056f7f02d7d988adf9337af
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-03-17 16:45:45 +0100

    added custom loader

---
+++ lua/qamar/loader.lua
@@ -0,0 +1,46 @@
+local codepoints = require('toolshed.util.string').codepoints
+local parser = require 'qamar.parser'
+local tokenizer = require 'qamar.tokenizer'
+local char_stream = require 'qamar.tokenizer.char_stream'
+
+local function create_parser(str)
+    return parser(tokenizer(char_stream(codepoints(str))))
+end
+
+local function get_runtime_paths()
+    local delimiter = ','
+    local ret = {}
+    for path in vim.o.runtimepath:gmatch('([^,]*)' .. delimiter) do
+        table.insert(ret, path)
+    end
+    return ret
+end
+
+return function(modulename)
+    modulename = string.gsub(modulename, '%.', '/')
+    for _, runtimepath in ipairs(get_runtime_paths()) do
+        local path = runtimepath .. '/lua/' .. modulename
+        local path2 = runtimepath .. '/lua/' .. modulename .. '/init.qamar'
+        path = path .. '.qamar'
+        local file = io.open(path, 'rb')
+        if not file then
+            path = path2
+            file = io.open(path, 'rb')
+        end
+        if file then
+            local str = file:read '*a'
+            file:close()
+            local chunk = create_parser(str).chunk()
+            if chunk then
+                local chunkstr = tostring(chunk)
+                if chunkstr then
+                    local loaded = load(chunkstr, path)
+                    if loaded then
+                        return loaded
+                    end
+                end
+            end
+        end
+    end
+    return ''
+end
--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -98,6 +98,8 @@ return {
             return tbl
         end
 
+        table.insert(package.loaders, 2, require 'qamar.loader')
+
         nnoremap(
             '<leader>cr',
             ":lua local to_unload = {} for k in pairs(package.loaded) do if k == 'qamar' or (#k >= 6 and k:sub(1, 6) == 'qamar.') then table.insert(to_unload, k) end end for _, k in ipairs(to_unload) do package.loaded[k] = nil end require'qamar'.run()<cr>",
