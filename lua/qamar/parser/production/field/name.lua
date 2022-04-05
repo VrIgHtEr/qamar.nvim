1c38fa193535a499ce2fb89ac0f8f2b407f765c3
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-04-05 16:07:02 +0200

    fully localized field parser

--- lua/qamar/parser/production/field/name.lua
+++ lua/qamar/parser/production/field/name.lua
@@ -9,6 +9,11 @@ local take = p.take
 local commit = p.commit
 local undo = p.undo
 
+local tname = token.name
+local tassignment = token.assignment
+local nfield_name = n.field_name
+local setmetatable = setmetatable
+
 local mt = {
     __tostring = function(self)
         return tconcat { self.key, '=', self.value }
@@ -17,15 +22,15 @@ local mt = {
 
 return function(self)
     local key = peek(self)
-    if key and key.type == token.name then
+    if key and key.type == tname then
         begin(self)
         local left = take(self).pos.left
         local tok = take(self)
-        if tok and tok.type == token.assignment then
+        if tok and tok.type == tassignment then
             local value = expression(self)
             if value then
                 commit(self)
-                local ret = setmetatable({ key = key.value, value = value, type = n.field_name, pos = { left = left, right = value.pos.right } }, mt)
+                local ret = setmetatable({ key = key.value, value = value, type = nfield_name, pos = { left = left, right = value.pos.right } }, mt)
                 return ret
             end
         end
--- lua/qamar/parser/production/field/raw.lua
+++ lua/qamar/parser/production/field/raw.lua
@@ -9,6 +9,11 @@ local begin = p.begin
 local take = p.take
 local commit = p.commit
 local undo = p.undo
+local tlbracket = token.lbracket
+local trbracket = token.rbracket
+local tassignment = token.assignment
+local nfield_raw = n.field_raw
+local setmetatable = setmetatable
 
 local mt = {
     __tostring = function(self)
@@ -17,19 +22,19 @@ local mt = {
 }
 return function(self)
     local tok = peek(self)
-    if tok and tok.type == token.lbracket then
+    if tok and tok.type == tlbracket then
         begin(self)
         local left = take(self).pos.left
         local key = expression(self)
         if key then
             tok = take(self)
-            if tok and tok.type == token.rbracket then
+            if tok and tok.type == trbracket then
                 tok = take(self)
-                if tok and tok.type == token.assignment then
+                if tok and tok.type == tassignment then
                     local value = expression(self)
                     if value then
                         commit(self)
-                        return setmetatable({ key = key, value = value, pos = { left = left, right = value.pos.right }, type = n.field_raw }, mt)
+                        return setmetatable({ key = key, value = value, pos = { left = left, right = value.pos.right }, type = nfield_raw }, mt)
                     end
                 end
             end
